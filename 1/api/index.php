04092ef45dd7c0ad0efc92f4af1b86a2fd24b3f2
igoritoc5 <108372437+igoritoc5@users.noreply.github.com>
2022-11-18 00:09:54 -0300

    Update index.php

--- 1/api/index.php
+++ 1/api/index.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -10,48 +13,38 @@ require 'SMTP.php';
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
         if (!empty($_GET["email"])) {
-
-            // Config
-
-            $mail = new PHPMailer();
-
-            $mail->IsSMTP();
-
-            $mail->CharSet = 'UTF-8';
-
-            $mail->Host       = "mail.outlook.com";               // SMTP server example
-
-            $mail->SMTPDebug  = 0;                                // enables SMTP debug information (for testing)
-
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;      //Enable implicit TLS encryption
-
-            $mail->SMTPAuth   = true;                             // enable SMTP authentication
-
-            $mail->Port       = 465;                              // set the SMTP port for the GMAIL server
-
-            $mail->Username   = "Pro-ti@hotmail.com";             // SMTP account username example
-
-            $mail->Password   = "123456789aA@";                   // SMTP account password example
-
-            //Recipients
-
-            $mail->setFrom('Pro-ti@hotmail.com', 'Mailer');
             
-            $mail->addAddress('igoritoc5@uni9.edu.br', 'Igor');   //Add a recipient
-        
-            $mail->addReplyTo($_GET["email"], 'Resposta');
-
-            // Content
-
-            $mail->isHTML(false);                                 // Set email format to HTML
+            $mail = new PHPMailer;
+            $mail->isSMTP();
+            $mail->Host = 'smtp-mail.outlook.com';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+            $mail->SMTPAuth = true;
+            $mail->Username = 'Pro-Ti@hotmail.com';
+            $mail->Password = '123456789aA@';
+            $mail->setFrom('Pro-Ti@hotmail.com', 'First Last');
+            $mail->addAddress("igoritoc5@uni9.edu.br", "Recepient Name");
+            $mail->addReplyTo($_GET["email"],"Reply");
+            $mail->isHTML(false);                                 
 
             $mail->Subject = 'Uma nova empresa entrou em contato.';
 
-            $mail->Body    = '';
+            $mail->Body    = 'O usuário ' . $_GET["nome"] . ' com telefone de contato: ' . $_GET["telefone"] . '. Entrou em contato em nome da empresa ' . $_GET["company"] . ' com  a mensagem: ' . $_GET["msg"];
+
 
             $mail->AltBody = 'O usuário ' . $_GET["nome"] . ' com telefone de contato: ' . $_GET["telefone"] . '. Entrou em contato em nome da empresa ' . $_GET["company"] . ' com  a mensagem: ' . $_GET["msg"];
 
-            $mail->send();
+            
+            if(!$mail->send()) 
+            {
+                echo "Mailer Error: " . $mail->ErrorInfo;
+            } 
+            else 
+            {
+                echo "Message has been sent successfully";
+            }
+             
+
         }
     }
 
